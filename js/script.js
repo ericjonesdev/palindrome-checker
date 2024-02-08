2a04c94507fa6314ea896acbfb8f787d26c3846f
Eric Nathaniel Jones <118446690+ericjonesdev@users.noreply.github.com>
2024-02-08 18:46:38 +0100

    add event listener for userDataInput and checkButton

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-const userData = document.getElementById('text-input');
+const userDataInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const palResult = document.getElementById('result');
 
+// add event listener for checkButton
+checkButton.addEventListener('click', () => {
+    checkForPalindrome(userDataInput.value);
+    userDataInput.value = '';
+})
+
+// add event listener for userDataInput
+
+userDataInput.addEventListener( e => {
+    if (e.key === 'ENTER') {
+        checkForPalindrome(userDataInput.value);
+        userDataInput.value = '';
+    }
+
+})
